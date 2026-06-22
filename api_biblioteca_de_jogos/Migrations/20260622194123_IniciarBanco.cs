fce1922dd0307dfa14377dd018114481cf7ec1d4
rehmeloz <renatamelo.pro@outlook.com>
2026-06-22 16:44:18 -0300

    adiciona migração inicial do banco

---
+++ api_biblioteca_de_jogos/Migrations/20260622194123_IniciarBanco.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_biblioteca_de_jogos.Migrations
+{
+    /// <inheritdoc />
+    public partial class IniciarBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jogos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "varchar", maxLength: 1, nullable: false),
+                    Genero = table.Column<string>(type: "varchar", maxLength: 50, nullable: false),
+                    Modo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Categoria = table.Column<int>(type: "INTEGER", nullable: false),
+                    Possui = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Nota = table.Column<int>(type: "INTEGER", nullable: false),
+                    Comentario = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogos");
+        }
+    }
+}
