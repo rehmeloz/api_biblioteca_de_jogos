2410bf035f710251b0455bf0d0503531813678ea
rehmeloz <renatamelo.pro@outlook.com>
2026-06-22 16:42:34 -0300

    adiciona appdbcontext e mapping da entidade jogo

---
+++ api_biblioteca_de_jogos/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using api_biblioteca_de_jogos.Data.Mapping;
+using api_biblioteca_de_jogos.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_biblioteca_de_jogos.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Jogo> Jogos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new JogoMapping());
+    }
+}
---
+++ api_biblioteca_de_jogos/Data/Mapping/JogoMapping.cs
@@ -0,0 +1,19 @@
+﻿using api_biblioteca_de_jogos.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace api_biblioteca_de_jogos.Data.Mapping;
+
+public class JogoMapping : IEntityTypeConfiguration<Jogo>
+{
+    public void Configure(EntityTypeBuilder<Jogo> builder)
+    {
+        builder.HasKey(i => i.Id);
+        builder.Property(n => n.Nome).HasColumnType("varchar").HasMaxLength(100).IsRequired();
+        builder.Property(g => g.Genero).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+        builder.Property(m => m.Modo).IsRequired();
+        builder.Property(c => c.Categoria).IsRequired();
+        builder.Property(n => n.Nome).HasMaxLength(1);
+        builder.Property(c => c.Comentario).HasMaxLength(150);
+    }
+}
