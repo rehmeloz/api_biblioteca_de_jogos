5a5ed199f74bc4b2a4cd6d4ccb1276e97205d5fe
rehmeloz <renatamelo.pro@outlook.com>
2026-06-22 15:55:32 -0300

    adiciona propriedades categoria, nota e comentário para jogo

--- api_biblioteca_de_jogos/Entities/Jogo.cs
+++ api_biblioteca_de_jogos/Entities/Jogo.cs
@@ -8,5 +8,8 @@ public class Jogo
     public string Nome { get; set; } = string.Empty;
     public string Genero { get; set; } = string.Empty;
     public EModo Modo { get; set; }
+    public ECategoria Categoria { get; set; }
     public bool Possui { get; set; }
+    public int Nota { get; set; }
+    public string Comentario { get; set; } = string.Empty;
 }
