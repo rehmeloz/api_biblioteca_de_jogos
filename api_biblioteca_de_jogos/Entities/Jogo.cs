2ef9b759d093dc843267f6a86781eb3c953bf94b
rehmeloz <renatamelo.pro@outlook.com>
2026-06-22 15:31:01 -0300

    primeiro commit

---
+++ api_biblioteca_de_jogos/Entities/Jogo.cs
@@ -0,0 +1,12 @@
+﻿using api_biblioteca_de_jogos.Enums;
+
+namespace api_biblioteca_de_jogos.Entities;
+
+public class Jogo
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Genero { get; set; } = string.Empty;
+    public EModo Modo { get; set; }
+    public bool Possui { get; set; }
+}
---
+++ api_biblioteca_de_jogos/Enums/EModo.cs
@@ -0,0 +1,7 @@
+﻿namespace api_biblioteca_de_jogos.Enums;
+
+public enum EModo
+{
+    Singleplayer,
+    Multiplayer
+}
