31616a5da33a02ca3b6dcbfa7b65d63adb15d64f
rehmeloz <renatamelo.pro@outlook.com>
2026-06-23 16:04:26 -0300

    adiciona camada service com IJogoService e JogoService

---
+++ api_biblioteca_de_jogos/Services/IJogoService.cs
@@ -0,0 +1,16 @@
+﻿using api_biblioteca_de_jogos.DTOs;
+using api_biblioteca_de_jogos.Entities;
+using api_biblioteca_de_jogos.Enums;
+
+namespace api_biblioteca_de_jogos.Services;
+
+public interface IJogoService
+{
+    public Task CadastrarJogo(Jogo jogo);
+    public Task EditarJogo(int id, EditarJogoDTO jogoEditado);
+    public Task ExcluirJogo(int id);
+    public Task BuscarPorId(int id);
+    public Task<List<Jogo>> ListarJogos(int pagina, int quantidade);
+    public Task<List<Jogo>> ListarPorCategoria(ECategoria categoria, int pagina, int quantidade);
+    public Task<List<Jogo>> ListaPorNota(int nota, int pagina, int quantidade);
+}
---
+++ api_biblioteca_de_jogos/Services/JogoService.cs
@@ -0,0 +1,70 @@
+﻿using api_biblioteca_de_jogos.DTOs;
+using api_biblioteca_de_jogos.Entities;
+using api_biblioteca_de_jogos.Enums;
+using api_biblioteca_de_jogos.Repositories;
+using SQLitePCL;
+
+namespace api_biblioteca_de_jogos.Services;
+
+public class JogoService : IJogoService
+{
+    public readonly IJogoRepository _repository;
+
+    public JogoService(IJogoRepository repository)
+    {
+        repository = _repository;
+    }
+
+    public async Task CadastrarJogo(Jogo jogo)
+    {
+        await _repository.CadastrarJogo(jogo);
+    }
+
+    public async Task EditarJogo(int id, EditarJogoDTO jogoEditado)
+    {
+        var jogo = _repository.BuscarPorId(id);
+
+        if (jogo == null)
+            throw new Exception("O jogo não foi encontrado!");
+
+        await _repository.EditarJogo(id, jogoEditado);
+    }
+
+    public async Task ExcluirJogo(int id)
+    {
+        var jogo = _repository.BuscarPorId(id);
+
+        if (jogo == null)
+            throw new Exception("O jogo não foi encontrado!");
+
+        await _repository.ExcluirJogo(id);
+    }
+
+    public async Task BuscarPorId(int id)
+    {
+        var jogo = _repository.BuscarPorId(id);
+
+        if (jogo == null)
+            throw new Exception($"O jogo de id {id} não foi encontrado!");
+
+        await _repository.BuscarPorId(id);
+    }
+
+    public async Task<List<Jogo>> ListarJogos(int pagina, int quantidade)
+    {
+       return await _repository.ListarJogos(pagina, quantidade);
+    }
+
+    public async Task<List<Jogo>> ListarPorCategoria(ECategoria categoria, int pagina, int quantidade)
+    {
+        return await _repository.ListarPorCategoria(categoria, pagina, quantidade);
+    }
+
+    public async Task<List<Jogo>> ListaPorNota(int nota, int pagina, int quantidade)
+    {
+        if (nota > 5)
+            throw new Exception("A nota não pode ser maior que 5!");
+
+        return await _repository.ListaPorNota(nota, pagina, quantidade);
+    }
+}
