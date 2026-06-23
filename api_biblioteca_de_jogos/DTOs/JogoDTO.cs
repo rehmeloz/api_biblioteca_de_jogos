5b278d67127baf21ad9cfb90076efb14fe3571e8
rehmeloz <renatamelo.pro@outlook.com>
2026-06-23 16:20:03 -0300

    muda de EditarJogoDTO para JogoDTO para poder ser usado nas demais camadas

---
+++ api_biblioteca_de_jogos/DTOs/JogoDTO.cs
@@ -0,0 +1,14 @@
+﻿using api_biblioteca_de_jogos.Enums;
+
+namespace api_biblioteca_de_jogos.DTOs;
+
+public class JogoDTO
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Genero { get; set; } = string.Empty;
+    public EModo Modo { get; set; }
+    public ECategoria Categoria { get; set; }
+    public bool Possui { get; set; }
+    public int Nota { get; set; }
+    public string Comentario { get; set; } = string.Empty;
+}
