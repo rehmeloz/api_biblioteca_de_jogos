1dd67593ad8fe64807970ac94ebd7501e8597d85
rehmeloz <renatamelo.pro@outlook.com>
2026-06-24 17:28:19 -0300

    adiciona retorno de somente task para task de jogo no método buscar jogo por id

--- api_biblioteca_de_jogos/Repositories/IJogoRepository.cs
+++ api_biblioteca_de_jogos/Repositories/IJogoRepository.cs
@@ -9,7 +9,7 @@ public interface IJogoRepository
     public Task CadastrarJogo(Jogo jogo);
     public Task EditarJogo(int id, Jogo jogo);
     public Task ExcluirJogo(int id);
-    public Task BuscarPorId(int id);
+    public Task<Jogo> BuscarPorId(int id);
     public Task<List<Jogo>> ListarJogos(int pagina, int quantidade);
     public Task<List<Jogo>> ListarPorCategoria(ECategoria categoria, int pagina, int quantidade);
     public Task<List<Jogo>> ListaPorNota(int nota, int pagina, int quantidade);
