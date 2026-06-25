a836ba6f1d826e389d27f71d1ddb5eaad260dd62
rehmeloz <renatamelo.pro@outlook.com>
2026-06-25 16:28:54 -0300

    adiciona migrations incluindo a adição do novo campo Media

---
+++ api_biblioteca_de_jogos/Migrations/20260625192430_AdicionaMedia.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_biblioteca_de_jogos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaMedia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Media",
+                table: "Jogos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Media",
+                table: "Jogos");
+        }
+    }
+}
