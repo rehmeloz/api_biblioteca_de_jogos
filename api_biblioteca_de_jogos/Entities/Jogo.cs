61ed8e3e80a3e1c4b3552f2719c0fd2b3890f4ed
rehmeloz <renatamelo.pro@outlook.com>
2026-06-25 17:02:38 -0300

    adiciona serialização dos enums Modo e  Categoria como strings

--- api_biblioteca_de_jogos/Entities/Jogo.cs
+++ api_biblioteca_de_jogos/Entities/Jogo.cs
@@ -1,4 +1,5 @@
 ﻿using api_biblioteca_de_jogos.Enums;
+using System.Text.Json.Serialization;
 
 namespace api_biblioteca_de_jogos.Entities;
 
@@ -7,7 +8,9 @@ public class Jogo
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
     public string Genero { get; set; } = string.Empty;
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public EModo Modo { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public ECategoria Categoria { get; set; }
     public bool Possui { get; set; }
     public int Nota { get; set; }
