9c9c4c9b6cd77f28fc3fb47c010bf995eca258b1
rehmeloz <renatamelo.pro@outlook.com>
2026-06-30 16:18:35 -0300

    Adiciona entidade Usuarios e DTO UsuariosDTO

---
+++ api_biblioteca_de_jogos/DTOs/UsuariosDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace api_biblioteca_de_jogos.DTOs;
+
+public class UsuariosDTO
+{
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
---
+++ api_biblioteca_de_jogos/Entities/Usuarios.cs
@@ -0,0 +1,9 @@
+﻿namespace api_biblioteca_de_jogos.Entities;
+
+public class Usuarios
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "User";
+}
