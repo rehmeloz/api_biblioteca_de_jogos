923c11f8a9342707ec717dcfcb981bb5e2ef7387
rehmeloz <renatamelo.pro@outlook.com>
2026-06-30 17:12:06 -0300

    Adiciona interface e service de Usuario

---
+++ api_biblioteca_de_jogos/Services/IUsuarioService.cs
@@ -0,0 +1,8 @@
+﻿using api_biblioteca_de_jogos.Entities;
+
+namespace api_biblioteca_de_jogos.Services;
+
+public interface IUsuarioService
+{
+    Usuario? ValidateUser(string username, string password);
+}
---
+++ api_biblioteca_de_jogos/Services/UsuarioService.cs
@@ -0,0 +1,24 @@
+﻿using api_biblioteca_de_jogos.Entities;
+using api_biblioteca_de_jogos.Repositories;
+
+namespace api_biblioteca_de_jogos.Services;
+
+public class UsuarioService : IUsuarioService
+{
+    private readonly IUsuarioRepository _repository;
+
+    public UsuarioService(IUsuarioRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Usuario? ValidateUser(string username, string password)
+    {
+        var user = _repository.GetByUsername(username);
+
+        if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.PasswordHash))
+            return null;
+
+        return user;
+    }
+}
