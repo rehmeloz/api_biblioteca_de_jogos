01110b38d7e97243e1530d873c6aaa78480d4559
rehmeloz <renatamelo.pro@outlook.com>
2026-06-30 17:09:26 -0300

    Ajusta nome de plural para singular

---
+++ api_biblioteca_de_jogos/DTOs/UsuarioDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace api_biblioteca_de_jogos.DTOs;
+
+public class UsuarioDTO
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
