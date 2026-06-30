7eb75a5bae4e4c80b19343ad379662bb1fd91c08
rehmeloz <renatamelo.pro@outlook.com>
2026-06-30 17:11:38 -0300

    Adiciona interface e repositório de Usuario

---
+++ api_biblioteca_de_jogos/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,8 @@
+﻿using api_biblioteca_de_jogos.Entities;
+
+namespace api_biblioteca_de_jogos.Repositories;
+
+public interface IUsuarioRepository
+{
+    Usuario? GetByUsername(string username);
+}
---
+++ api_biblioteca_de_jogos/Repositories/UsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using api_biblioteca_de_jogos.Data;
+using api_biblioteca_de_jogos.Entities;
+
+namespace api_biblioteca_de_jogos.Repositories;
+
+public class UsuarioRepository : IUsuarioRepository
+{
+    private readonly AppDbContext _context;
+
+    public UsuarioRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Usuario? GetByUsername(string username)
+    {
+        return _context.Usuarios.SingleOrDefault(u => u.Username == username);
+    }
+}
