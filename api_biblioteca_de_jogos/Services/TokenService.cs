510db5e3db2a0ad85b89e1c84be82d32547a6866
rehmeloz <renatamelo.pro@outlook.com>
2026-07-01 16:17:28 -0300

    Adiciona implementação completa GenerateToken no TokenService

--- api_biblioteca_de_jogos/Services/TokenService.cs
+++ api_biblioteca_de_jogos/Services/TokenService.cs
@@ -1,4 +1,8 @@
 ﻿using api_biblioteca_de_jogos.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace api_biblioteca_de_jogos.Services;
 
@@ -13,6 +17,26 @@ public class TokenService : ITokenService
 
     public string GenerateToken(Usuario usuario)
     {
-        return "";
+        var claims = new[]
+{
+            new Claim(ClaimTypes.NameIdentifier, usuario.Id.ToString()),
+            new Claim(ClaimTypes.Name, usuario.Username),
+            new Claim(ClaimTypes.Role, usuario.Role)
+        };
+
+        var key = new SymmetricSecurityKey(
+        Encoding.ASCII.GetBytes(_config["Jwt:Key"]));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(8),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
