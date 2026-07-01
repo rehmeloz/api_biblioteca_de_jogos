0fcd5cc2442c51a99aed67502457dbc02293077a
rehmeloz <renatamelo.pro@outlook.com>
2026-07-01 16:11:02 -0300

    Adiciona interface e service para Token

---
+++ api_biblioteca_de_jogos/Services/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using api_biblioteca_de_jogos.Entities;
+
+namespace api_biblioteca_de_jogos.Services;
+
+public interface ITokenService
+{
+   string GenerateToken(Usuario usuario);
+}
---
+++ api_biblioteca_de_jogos/Services/TokenService.cs
@@ -0,0 +1,18 @@
+﻿using api_biblioteca_de_jogos.Entities;
+
+namespace api_biblioteca_de_jogos.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly IConfiguration _config;
+
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public string GenerateToken(Usuario usuario)
+    {
+        return "";
+    }
+}
